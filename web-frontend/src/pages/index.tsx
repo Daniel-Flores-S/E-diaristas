2859fc13ff7c871ed54575d998c7016d26661121
Daniel <danielfloresda802@gmail.com>
2021-06-12 19:17:22 -0300

    Creating component user information

--- web-frontend/src/pages/index.tsx
+++ web-frontend/src/pages/index.tsx
@@ -1,15 +1,23 @@
 import PageTitle from 'ui/components/data-display/PageTitle';
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment';
+import UserInformetion from   'ui/components/data-display/UserInformetion';
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
+      <PageTitle
         title={"Conheça nossos profissionais."}
         subtitle={"Preencha seu Endereço é veja todos os proficionais da sua localidade"}
       />
+      <UserInformetion
+        picture={"https://github.com/Daniel-Flores-S.png"}
+        name={"Daniel"}
+        rating={3}
+        description={"Bahia"}
+
+      />
     </div>
-    
+
   )
 }
--- web-frontend/src/ui/components/data-display/UserInformetion/index.tsx
+++ web-frontend/src/ui/components/data-display/UserInformetion/index.tsx
@@ -1,8 +1,29 @@
-import React from "react"
-const UserInformetion: React.FC = () =>  {
-    return(
-        
-);
+import React from "react";
+
+import {
+    UserInformetionContainer,
+    UserName,
+    UserDescription,
+    AvatarStyled,
+    RatingStyled,
+} from "./styles";
+
+interface UserInformetionProps {
+    picture: string;
+    name: string;
+    rating: number;
+    description?: string;
 }
+
+const UserInformetion: React.FC<UserInformetionProps> = (props) => {
+    return (
+        <UserInformetionContainer>
+            <AvatarStyled src={props.picture} />
+            <UserName> {props.name}</UserName>
+            <RatingStyled readOnly value={props.rating} />
+            <UserDescription>{props.description}</UserDescription>
+        </UserInformetionContainer>
+    );
+};
 
 export default UserInformetion;
