993b9ff8ded5fe6c93e09a439e99402342b2e62c
Daniel <danielfloresda802@gmail.com>
2021-06-16 17:23:26 -0300

    Creating templates for day laborers' web administration

--- py-backend/ediaristas/web/urls.py
+++ py-backend/ediaristas/web/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import cadastrar_diaristas
+from .views import cadastrar_diaristas, listar_diaristas
+
 urlpatterns = [
-    path('cadastrar_diaristas', cadastrar_diaristas, name='cadastrar_diaristas')
+    path('cadastrar_diaristas', cadastrar_diaristas, name='cadastrar_diaristas'),
+    path('listar_diaristas', listar_diaristas, name='listar_diaristas')
 ]
